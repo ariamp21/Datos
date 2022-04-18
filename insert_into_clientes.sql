3d058bc00cabd69ea2d30ca074f291dd814812a4
ariamp21 <48859269+ariamp21@users.noreply.github.com>
2022-04-18 11:28:17 -0500

    Update insert_into_clientes.sql
    Insertando más filas según columnas especificadas

--- insert_into_clientes.sql
+++ insert_into_clientes.sql
@@ -1,4 +1,14 @@
 INSERT INTO CLIENTES (ID,NOMBRE,PAIS,CATEGORIA,NUM_VENTA)
-VALUES(1, 'Ariam Paz Aranda Reyes', 'Chile', 'Eco', 3);
+VALUES(1, 'Ariam Aranda Reyes', 'Chile', 'Libros', 3),
+	  (2, 'Amaru Fernandez Durán', 'Chile', 'Tecnología',6),
+	  (3, 'Diana López Áviles','Alemania','Robótica',10),
+	  (4, 'Constanza Tobar Riquelme','China','Música',7),
+	  (5,'Claudia Cordero Solis', 'Alemania', 'Entretención',9),
+	  (6, 'Victoria Aravena Opazo','China','Eco',5),
+	  (7, 'Sally Aranda Reyes','España','Accesorios',9),
+	  (8, 'Cecilia Reyes Vargas','Italia','Entretención',10),
+	  (9, 'Ada Wynn Lunt','Mexico','Libros',5),
+	  (10, 'Alicia Aranda Reyes','Chile','Mascotas',11),
+	  (11,'Hilda Vicencio Serey','Perú','Entretención',4);
 
 select * from CLIENTES
